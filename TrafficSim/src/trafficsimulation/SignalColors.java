df16540978fbed0729ef1397c8d92085ec100e15
Andrew Petriccione <apetricc@unca.edu>
2016-09-08 10:24:06 -0400

    No comments, but add message to prompt for enter key to draw/erase. Still need to check for final cleanup and double check with UML.

--- TrafficSim/src/trafficsimulation/SignalColors.java
+++ TrafficSim/src/trafficsimulation/SignalColors.java
@@ -1,5 +1,11 @@
 package trafficsimulation;
 
-public class SignalColors {
-
+public enum SignalColors {
+	GREEN_RED, YELLOW_RED, RED_GREEN, RED_YELLOW;
+	
+	private static SignalColors[] vals = values();
+	
+	public SignalColors next() {
+		return vals[(this.ordinal()+1) % vals.length];
+	}
 }
