7ed682c2ad6df2cd7c489bb02d650c115bbd2f57
Andrew Petriccione <apetricc@unca.edu>
2016-09-26 23:28:31 -0400

    Have all the methods for SimpleString but not sure all the JUnit tests are correct or testing what I think they are...

--- SimpleStringHW2/src/SimpleStringPackage/SimpleString.java
+++ SimpleStringHW2/src/SimpleStringPackage/SimpleString.java
@@ -1,6 +1,8 @@
 package SimpleStringPackage;
 import java.util.Arrays;
 
+import org.junit.Before;
+
 public class SimpleString {
 	char[] contents;
 	private int length;
@@ -37,7 +39,7 @@ hashCode(): returns a hash code for this SimpleString.
  * @return
  */
 	
-	public int charAt(int index) {
+	public char charAt(int index) {
 		return this.contents[index];
 	}
 	
@@ -64,6 +66,89 @@ hashCode(): returns a hash code for this SimpleString.
 		return false;
 	}//isEmpty
 	
+	public int length() {		
+		return this.length;
+	}
+	
+	public SimpleString concat(SimpleString s) {
+		char[] letters = new char[this.length + s.length];
+		SimpleString combined;
+		for (int i = 0; i < this.length; i++) {
+			letters[i] = this.charAt(i);
+			//letters[i+this.length-1] = s.charAt(i);
+		}		
+		for (int j = 0; j < s.length; j++) {
+			letters [j+this.length] = s.charAt(j);
+		}
+		combined = new SimpleString(letters); 
+		return combined;
+	}
+	
+	public String printConcat(SimpleString second) {
+		String result = "";
+		for (int i = 0; i < (this.length()+ second.length()); i++) {
+			result += (this.concat(second).charAt(i));
+		}
+		return result;
+	}//printConcat
+
+	/**
+	 * substring(int startIndex, int endIndex): returns a new SimpleString that is a copy 
+	 * of the portion of this SimpleString that starts at index startIndex and ends at index endIndex - 1.
+	 * @param startIndex
+	 * @param endIndex
+	 * @return
+	 */
+	public SimpleString substring(int startIndex, int endIndex) {
+		SimpleString substring;
+		char[] substringChars = new char[Math.abs(endIndex-startIndex)];
+		for (int i = 0; i < substringChars.length; i++) {
+			substringChars[i] = this.charAt(i + startIndex);
+		}
+		substring = new SimpleString(substringChars);
+		return substring;
+	}//substring
+	
+//	equals(Object o): returns true if this SimpleString is equal to o.
+  public boolean equals(Object o) {
+	  return (this == o);
+  }
+  
+//	hashCode(): returns a hash code for this SimpleString.
+  @Override
+  public int hashCode() {
+	  int hashCode = 7;
+	  int i = 1;
+	  int limit = (contents != null) ? contents.length : 0;
+	  
+		  while (i < limit) {
+			  hashCode = 31 * Integer.hashCode(contents.length + i);
+			  i++;
+		  }  
+	  
+	  hashCode = 31 * Arrays.hashCode(contents);
+	  
+	  return hashCode;
+  }
+	public static void main(String[] args) {
+		char[] chars = {'w', 'o', 'r', 'd'};
+		char[] noChars = {};
+		char[] addOnWord = {'c','a','t'};
+		SimpleString testString;
+		SimpleString emptyString;
+		SimpleString addOnString;
+			testString = new SimpleString(chars);
+			emptyString = new SimpleString(noChars);
+			addOnString = new SimpleString(addOnWord);
+		
+		System.out.println(testString.printConcat(addOnString));
+		System.out.println(testString.equals(testString));
+		System.out.println(testString.equals(emptyString));
+		System.out.println(testString.hashCode());
+		System.out.println(testString.hashCode());
+		System.out.println(emptyString.hashCode());
+	}
+
 	
 	
-}
+}//SimpleString
--- SimpleStringHW2/src/SimpleStringPackage/TesterTest.java
+++ SimpleStringHW2/src/SimpleStringPackage/TesterTest.java
@@ -12,52 +12,56 @@ public class TesterTest {
 
 	char[] chars = {'w', 'o', 'r', 'd'};
 	char[] noChars = {};
+	char[] addOnWord = {'c','a','t'};
 	SimpleString testString;
 	SimpleString emptyString;
-	//SimpleString testString = new SimpleString(chars);
+	SimpleString addOnString;
+	SimpleString testString2;
 	
 	@Before
 	public void setUp() {
-		 testString = new SimpleString(chars);
-			emptyString = new SimpleString(noChars);
+		testString = new SimpleString(chars);
+		emptyString = new SimpleString(noChars);
+		addOnString = new SimpleString(addOnWord);
+		testString2 = new SimpleString(chars);
 	}
 	
-
+@After
 	@Test
 	public void testCharAt() {
 
-		assertEquals(testString.charAt(0), 'w');
-		assertEquals(testString.charAt(1), 'o');
-		assertEquals(testString.charAt(2), 'r');
-		assertEquals(testString.charAt(3), 'd');
+		assertEquals('w', testString.charAt(0));
+		assertEquals('o', testString.charAt(1));
+		assertEquals('r', testString.charAt(2));
+		assertEquals('d', testString.charAt(3));
 		try{
 			testString.charAt(chars.length);
 			fail("Tried to pull a charAt index out of bounds");
 			
 		} catch (ArrayIndexOutOfBoundsException e) {
-			assertTrue("Array out of bounds here",true);
+			assertTrue("Array out of bounds here", true);
 		}
 	}//testCharAt
 	
-	
+	//assertEquals(double expected, double actual) 
 	
 	@Test
 	public void testContains() {
 		assertEquals(true,testString.contains('w'));
-		assertEquals(testString.contains('o'),true);
-		assertEquals(testString.contains('r'),true);
-		assertEquals(testString.contains('d'),true);
-		assertEquals(testString.contains('z'),false);
-		assertEquals(testString.contains('1'), false);
+		assertEquals(true, testString.contains('o'));
+		assertEquals(true,testString.contains('r'));
+		assertEquals(true,testString.contains('d'));
+		assertEquals(false,testString.contains('z'));
+		assertEquals(false,testString.contains('1'));
 		
 	}
 	
 	@Test
 	public void testIndexOf() {
-		assertEquals(testString.indexOf('w'), 0);
-		assertEquals(testString.indexOf('o'), 1);
-		assertEquals(testString.indexOf('r'), 2);
-		assertEquals(testString.indexOf('d'), 3);		
+		assertEquals(0, testString.indexOf('w'));
+		assertEquals(1,testString.indexOf('o'));
+		assertEquals(2,testString.indexOf('r'));
+		assertEquals(3,testString.indexOf('d'));		
 	}
 	/**
 	 * isEmpty(): returns true if the string contains no characters, false otherwise.
@@ -76,6 +80,41 @@ expected, actual
 	@Test
 	public void testIsEmpty() {
 		assertEquals( true, emptyString.isEmpty());
+		assertEquals(false, testString.isEmpty());
+	}
+	
+	@Test
+	public void testLength() {
+		assertEquals(4, testString.length());
+		assertEquals(0, emptyString.length());
+	}
+	
+
+	@Test
+	public void testConcat() {
+		assertEquals('c', testString.concat(addOnString).charAt(4));
+		assertEquals('a', testString.concat(addOnString).charAt(5));
+		assertEquals('t', testString.concat(addOnString).charAt(6));
+		assertEquals('w', testString.concat(addOnString).charAt(0));
+		assertEquals(7, testString.concat(addOnString).length());
+		
+	}
+	
+	@Test
+	public void testSubstring() {
+		assertEquals(3, testString.substring(0, 3).length());
+		assertEquals('o', testString.substring(1, 3).charAt(0));
+	}
+	
+	@Test
+	public void testEquals() {
+		assertEquals(true, testString.equals(testString));
+		assertEquals(false, testString.equals(addOnString));
+	}
+	
+	@Test
+	public void testHashCode() {
+		assertEquals(141947605, testString.hashCode());
 	}
 	
 
